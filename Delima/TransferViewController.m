0fee4cee637516f0ada976ff5d4ce38851e5de48
Arie <prasetyo.mailbox@gmail.com>
2014-12-23 00:11:43 +0700

    update

--- Delima/TransferViewController.m
+++ Delima/TransferViewController.m
@@ -68,7 +68,7 @@
 - (IBAction)postToServer:(id)sender {
     _user = [User getUserProfile];
     NSDictionary *params = @{@"uname":_user.uname,
-                             @"passwd":[[DelimaCommonFunction sharedCommonFunction]md5:_user.passwd],
+                             @"passwd":_user.passwd,
                              @"merCode":_user.merchantCode,
                              @"mernumber":_user.merNumber,
                              @"terminal":_user.terminal,
